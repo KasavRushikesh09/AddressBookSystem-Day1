d55a02c8d861d9f286c66b01e9f42a724287a065
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-12-23 13:40:07 +0530

    Add AddressBookMain

--- src/AddressBookSystem/AddressBookSystem.java
+++ src/AddressBookSystem/AddressBookSystem.java
@@ -1,4 +1,20 @@
 package AddressBookSystem;
 
+import java.util.HashMap;
+
 public class AddressBookSystem {
+
+    private HashMap<String ,AddressBook> system = new HashMap<>();
+    public void addAddressBook(String name) {
+        if (!system.containsKey(name)) {
+            system.put(name, new AddressBook());
+            System.out.println("Address Book created: " + name);
+        } else {
+            System.out.println("Address Book already exists");
+        }
+    }
+        public AddressBook getAddressBook (String name){
+            return system.get(name);
+        }
+    }
 }
